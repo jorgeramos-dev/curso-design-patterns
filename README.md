fc5701018ef062216a7add2a6791152d6622b91e
Jorge Ramos <142628950+jorgeramos-dev@users.noreply.github.com>
2024-03-20 18:19:55 -0300

    curso designet patterns

---
+++ README.md
@@ -0,0 +1,32 @@
+# Padrões de Projeto com PHP 8
+
+## 💻 Curso: Design Patterns (Padrões de Projeto com PHP 8)
+
+- Código do curso de Design Patterns da EspecializaTi Academy - Prof. Carlos Ferreira.
+
+☑️ Curso feito com o objetivo de se aprofundar em Design Patterns (Padrões de Projeto com PHP 8), procurando entender sobre cada conceitos e seguindo as melhores práticas para desenvolvimento.
+
+### Passo a passo para rodar o projeto
+Clone Repositório
+```sh
+git clone https://github.com/jorgeramos-dev/curso-design-patterns.git
+```
+
+```sh
+cd curso-design-patterns
+```
+
+Acessar o container
+```sh
+docker-compose exec app bash
+```
+
+
+Instalar as dependências do projeto
+```sh
+composer install
+```
+
+
+Acessar o projeto
+[http://localhost:8989](http://localhost:8989)
---
+++ public/index.php
@@ -0,0 +1,77 @@
+<?php
+
+// use Core\Creational\Builder\Practical\Address;
+// use Core\Creational\Builder\Practical\Enums\Role;
+// use Core\Creational\Builder\Practical\Phone;
+// use Core\Creational\Builder\Practical\User;
+
+use Core\Creational\Builder\Practical\Enums\Role;
+use Core\Creational\Builder\Practical\UserBuilder;
+use Core\Creational\Singleton\Practical\DbConnection;
+use Core\Creational\Singleton\Conceptual\Singleton;
+
+require_once '../vendor/autoload.php';
+
+// $carlosFounder = new User(
+//     firstName: 'Carlos',
+//     lastName: 'Ferreira',
+//     email: 'carlos@especializati.com.br',
+//     age: 29,
+//     role: Role::F,
+// );
+// $carlosFounder->setAddress(
+//     new Address(
+//         street: 'Rua x',
+//         city: 'City X',
+//         state: 'State X',
+//         postalCode: 5757009,
+//         country: 'BR',
+//     )
+// );
+// $carlosFounder->setPhone(
+//     new Phone(
+//         ddd: 64,
+//         number: 981701406,
+//     )
+// );
+// $user = (new UserBuilder)
+//             ->addBasicInfo(
+//                 firstName: 'Carlos',
+//                 lastName: 'Ferreira',
+//                 email: 'carlos@especializati.com.br',
+//                 age: 29,
+//                 role: Role::F,
+//             )
+//             ->addAddress(
+//                 street: 'Rua x',
+//                 city: 'City X',
+//                 state: 'State X',
+//                 postalCode: 5757009,
+//                 country: 'BR',
+//             )
+//             ->addPhone(
+//                 ddd: 64,
+//                 number: 981701406,
+//             )
+//             ->build();
+
+/**
+ * Singleton Conceitual
+ */
+// $instanceA = Singleton::getInstance();
+// $instanceB = Singleton::getInstance();
+// var_dump($instanceA === $instanceB);
+
+// $instance = DbConnection::getInstance();
+// DbConnection::getInstance();
+// DbConnection::getInstance();
+// DbConnection::getInstance();
+// DbConnection::getInstance();
+// DbConnection::getInstance();
+
+DbConnection::getConnection();
+DbConnection::getConnection();
+DbConnection::getConnection();
+DbConnection::getConnection();
+DbConnection::getConnection();
+DbConnection::getConnection();
